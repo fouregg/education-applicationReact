1389e3d31b79b45fef6f2ce36270ed46dd889c52
WorkStation <alexey.belousov.site@gmail.com>
2024-10-30 20:35:14 +0300

    Lesson 2. Customisation card

--- src/components/UserCard/UserCard.js
+++ src/components/UserCard/UserCard.js
@@ -1,27 +1,60 @@
 import React from 'react';
 import Card from 'react-bootstrap/Card';
 import ListGroup from 'react-bootstrap/ListGroup';
+import { EnvelopeAtFill, Phone, PersonWorkspace } from 'react-bootstrap-icons'; 
+import './UserCard.css';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 const user = {
-   username: "AlexGreetBoom",
+   email: "alex@gmail.com",
    firstName: "Alex",
    secondName: "Boom",
    age: 25,
-   date_registration: "10-10-2010",
-   date_lastComing: "29-10-2024",
+   phone: "+7(800)-555-35-35",
+   experience: 4,
    image_url: "",
 }
 
 function UserCard(){
    return(
-      <Card style={{ width: '18rem' }}>
-      <Card.Img variant="top" src="../../../logo192.png"  height={200} />
+      <Card className='userCard'>
+      <Card.Img className='imgCard p-5 mb-0' variant="top" src="../../../logo192.png"  height={100} />
       <Card.Body>
-        <Card.Title>{user.username}</Card.Title>
-        <Card.Subtitle className='my-3'>First Name: {user.firstName} <br></br>Second Name: {user.secondName}</Card.Subtitle>
+        <Card.Title className='mb-2'>
+          {user.firstName} {user.secondName}
+        </Card.Title>
+        <Card.Subtitle className='mb-1'>
+          <Row className='m-2'>
+            <Col sm={4}>
+              <EnvelopeAtFill size={30}/> 
+            </Col>
+            <Col sm={8} className='mt-1'>
+              {user.email}
+            </Col>
+          </Row>
+        </Card.Subtitle>
         <ListGroup variant="flush">
-          <ListGroup.Item><b>Date registration: </b>{user.date_registration}</ListGroup.Item>
-          <ListGroup.Item><b>Date last seen: </b>{user.date_lastComing}</ListGroup.Item>
+          <ListGroup.Item>
+            <Row>
+              <Col sm={2}>
+                <Phone size={20}/> 
+              </Col>
+              <Col sm={10} className='mt-1'>
+                <b>{user.phone}</b>
+              </Col>
+            </Row>
+          </ListGroup.Item>
+          <ListGroup.Item>
+            <Row>
+              <Col sm={2}>
+                <PersonWorkspace size={20}/> 
+              </Col>
+              <Col sm={10} className='mt-1'>
+                <b>Experience: {user.experience} years</b>
+              </Col>
+            </Row>
+          </ListGroup.Item>
           <ListGroup.Item><b>Age: </b>{user.age}</ListGroup.Item>
         </ListGroup>
       </Card.Body>
